4a48660ff3f7b9951f8d74deba414e1f81b019e8
Vanderson Andrade <vanderson@aluno.unilab.edu.br>
2022-09-19 18:06:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Dimension Scan</title>
+</head>
+<body>
+    <div id="all">
+
+        <div id="header-wrapper">
+            <header class="header-main">
+                <div class="logo"><a href=""><h1>Dimension Scan</h1></a></div>
+                <nav class="nav-main">
+                    <ul>
+				        <li>
+					        <a href="">Home</a>
+				        </li>
+				        <li>
+					        <a href="">Projetos</a>
+				        </li>
+				        <li>
+					        <a href="">Equipe</a>
+				        </li>
+				        <li id="btn-top">
+					        <a href="">Discord</a>
+				        </li>
+			        </ul>
+                </nav>
+            </header>
+        </div><!-- ## header wrapper -->
+        <div class="line"></div>
+        <div id="contents-wrapper">
+            <div>
+                
+                <section class="works-exhibitor">
+                    <h2>Projetos Principais</h2>
+                    <img class="work-1" src="img/1.jpg" alt="">
+                    <img class="work-2"src="img/2.jpg" alt="">
+                    <img class="work-3"src="img/3.jpg" alt="">
+                    <img class="work-4"src="img/4.jpg" alt="">
+                    <img class="work-5"src="img/5.jpg" alt="">
+                </section>
+            </div>
+            
+
+        </div><!-- ## contents wrapper -->
+        <div class="line"></div>
+
+        <div id="footer-wrapper">
+            <footer class="footer-main">
+                <p class="logo"><a href="">Dimension Scan</a></p>
+                <div class="footer-info">
+                    <aside class="social">redes</aside>
+                    <aside class="contact">contatos</aside>
+                </div>
+                <p>&copy; Todos os direitos reservados - By Vanderson Andrade</p>
+            </footer>
+        </div><!-- ## footer wrapper -->
+
+    </div>
+</body>
+</html>
